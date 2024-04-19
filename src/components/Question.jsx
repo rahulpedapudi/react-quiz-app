33a6faca9ea08a9e6037370aa7b52b4f67c351d9
Rahul <rahulpedapudi.01@gmail.com>
2024-04-19 21:23:10 +0530

    add comments and minor fixes

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,27 +5,35 @@ import { useEffect, useState } from "react";
 import Score from "./Scores";
 
 export default function Question() {
+  // State management for generating question.
+  // initial question is a random question from the data.
   const [question, setQuestion] = useState(() => {
     const index = Math.floor(Math.random() * results.length);
     return results[index];
   });
+  // Answers for the current question, (correct answer and incorrect answers are different key value pairs )
   const [answer, setAnswer] = useState([]);
+  // set user selected answer
   const [selectedAnswer, setSelectedAnswer] = useState(null);
+  // to check if answer is correct or incorrect
   const [isCorrect, setIsCorrect] = useState(null);
+  // keeping track of score
   const [score, setScore] = useState(0);
 
+  // function to get random object i.e.., question from db
   const getRandom = (results) => {
     const index = Math.floor(Math.random() * results.length);
     return results[index];
   };
 
+  // shuffles answers and sets the answer whenever the question state changes (dependency)
   useEffect(() => {
     const allAnswers = [...question.incorrect_answers, question.correct_answer];
-
     const shuffled = shuffleAnswers(allAnswers);
     setAnswer(shuffled);
   }, [question]);
 
+  // shuffle algorithm fisher-yates shuffle
   const shuffleAnswers = (array) => {
     const shuffled = [...array];
     for (let i = shuffled.length - 1; i > 0; i--) {
@@ -35,11 +43,13 @@ export default function Question() {
     return shuffled;
   };
 
+  // id is the answer string that user selected
   function handleClick(e) {
     const { id } = e.target;
     setSelectedAnswer(id);
   }
 
+  // checking if user selected answer is equal to actual answer
   function handleSubmit(e) {
     e.preventDefault();
     if (selectedAnswer === question.correct_answer) {
@@ -48,8 +58,13 @@ export default function Question() {
     } else {
       setIsCorrect(false);
     }
-    setQuestion(getRandom(results));
+    setQuestion(getRandom(results)); // generating another ques after submit
     setSelectedAnswer(null);
+    // uncheck radio buttons after submit
+    const radioButtons = document.querySelectorAll('input[type="radio"]');
+    radioButtons.forEach((button) => {
+      button.checked = false;
+    });
   }
 
   return (
